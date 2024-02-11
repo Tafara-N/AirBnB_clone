57f8396f3f3fdb1d2f0c58ef9203d105840d9921
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-11 13:35:14 +0200

    Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Amenity class.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    AirBnB amenities.
+
+    Attributes:
+        name (str): Amenity name
+    """
+
+    name = ""
