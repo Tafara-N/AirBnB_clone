7fb7bce286cfb02070ef57c62462e47b0b57e4f3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-11 13:36:23 +0200

    State class

---
+++ models/state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+State class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    The state
+
+    Attributes:
+        name (str): State's name
+    """
+
+    name = ""
